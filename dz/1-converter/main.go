779d414321fb1a4ea98706d9fe0aa24f84aa3e88
Artem Zhurkin <jurkin@axlebolt.com>
2025-07-09 15:07:37 +0300

    1-start

--- dz/1-converter/main.go
+++ dz/1-converter/main.go
@@ -2,9 +2,10 @@ package main
 
 import "fmt"
 
+const usdToEur = 0.85
+const usdToRub = 78.49
+
 func main() {
-	const usdToEur = 0.85
-	const usdToRub = 78.49
 	eurToRub := usdToRub / usdToEur
 
 	fmt.Printf("Курс EUR к RUB: %.2f\n", eurToRub)
