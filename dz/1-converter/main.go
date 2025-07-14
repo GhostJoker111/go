6f359502b4629dd70cebd2dce0c3a70fd1edc58f
Artem Zhurkin <jurkin@axlebolt.com>
2025-07-14 18:44:35 +0300

    1-start

--- dz/1-converter/main.go
+++ dz/1-converter/main.go
@@ -8,8 +8,18 @@ const usdToRub = 78.49
 var eurToRub = usdToRub / usdToEur
 
 func main() {
+	getUserInput()
 	fmt.Printf("Курс EUR к RUB: %.2f\n", eurToRub)
 }
 
-func getUserInput(value float64, curFrom, curTo string) float64 {
+func getUserInput() {
+	var currencyAmount float64
+	var currencyFrom, currencyTo string
+
+	fmt.Print("Введите количество валюты")
+	fmt.Scan(&currencyAmount)
+	fmt.Print("Введите из какой валюты нужно конвертировать?")
+	fmt.Scan(&currencyFrom)
+	fmt.Print("Введите в какую валюты нужно конвертировать?")
+	fmt.Scan(&currencyTo)
 }
