09dbad32adb46beafecd4242d6d5b10932bf42f8
Artem Zhurkin <jurkin@axlebolt.com>
2025-10-05 20:34:42 +0300

    3-struct(BINS)

--- dz/3-struct/main.go
+++ dz/3-struct/main.go
@@ -1,26 +1,5 @@
 package main
 
-import "time"
-
-type Bin struct {
-	Id        string    `json:"id"`
-	Private   bool      `json:"private"`
-	CreatedAt time.Time `json:"created_at"`
-	Name      string    `json:"name"`
-}
-
-func newBin(id string, private bool, createdAt time.Time, name string) (*Bin, error) {
-	newBin := &Bin{Id: id, Private: private, CreatedAt: createdAt, Name: name}
-	return newBin, nil
-}
-
-type BinList []Bin
-
-func createBin(len int) BinList {
-	binList := make(BinList, len)
-	return binList
-}
-
 func main() {
 
 }
--- dz/3-struct/storage/storage.go
+++ dz/3-struct/storage/storage.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"dz-3/struct/bins"
 	"dz-3/struct/files"
 	"encoding/json"
 
@@ -8,7 +9,7 @@ import (
 )
 
 type Storage struct {
-	Bins []Bin
+	Bins bins.BinList
 }
 
 func (storage *Storage) ToBytes() ([]byte, error) {
@@ -19,7 +20,24 @@ func (storage *Storage) ToBytes() ([]byte, error) {
 	return fileToBytes, nil
 }
 
-func (storage *Storage) save() {
+func (storage *Storage) Read() *Storage {
+	_, err := storage.ToBytes()
+	if err != nil {
+		color.Red("Не удалось преобразовать")
+	}
+	file, _ := files.ReadFile("data.json")
+	unmarshalErr := json.Unmarshal(file, &storage)
+	if unmarshalErr != nil {
+		color.Red("Не удалось разобрать файл data.json")
+		return &Storage{
+			Bins: bins.BinList{},
+		}
+	}
+
+	return storage
+}
+
+func (storage *Storage) Save() {
 	data, err := storage.ToBytes()
 	if err != nil {
 		color.Red("Не удалось преобразовать")
